da9667e0c2825ee3f5c9f4e9ef6a3291aac5d295
Artur Cabral <arturcabralreis@gmail.com>
2022-01-21 22:12:02 -0300

    add comentarios

--- fitaled.py
+++ fitaled.py
@@ -5,24 +5,18 @@
 fita1 = [10,0,0,0,0,0,0,0]
 #print(fita1)
 
-for n, i in enumerate(fita1):
-    if not i == 0:
+for n, i in enumerate(fita1): #varre a lista
+    if not i == 0: # procura onde está o pixel
         valTemp = fita1[n]
         fita1[n] = 0
         #print("achei")
         #print(n)
 
         
-        if (len(fita1) -1 > n):   
+        if (len(fita1) -1 > n):   # passa para proxima casa e diminui o peso
             if valTemp > 1:
                 fita1[n+1] = valTemp - 1
             else:
                 fita1[n+1] =  0
 
         print(fita1)    
-
-
-
-
-
-# diminui o peso da lista
